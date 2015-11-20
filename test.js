8cf681e15c11a03fab6dada176b8645f826c050b
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 08:33:11 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Uses Babel

---
+++ test.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var sass = require('node-sass');
+
+describe('theme-base', function() {
+  it('compiles ok', function(done) {
+    fs.readFile(`${__dirname}/theme.scss`, 'utf8', function(err, content) {
+      var results = sass.renderSync({
+        data: content
+      });
+      if(!results.css.length) throw new Error("Expecting results");
+      done();
+    });
+  });
+});
--- test/theme.spec.coffee
+++
@@ -1,9 +0,0 @@
-fs = require 'fs'
-sass = require 'node-sass'
-
-describe 'theme-base', ->
-  it 'compiles ok', (done) ->
-    fs.readFile "#{__dirname}/../scss/theme.scss", 'utf8', (err, content) ->
-      css = sass.renderSync data: content
-      throw new Error "Failed to compile" unless css?.length > 0
-      done()
